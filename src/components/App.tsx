792d14d37bf6adedd679b4699905b4498dd40960
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-12-30 14:17:26 +0200

    refactor : add gsap and parallax header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,39 @@
 import React from "react";
 import "../styles/index.scss";
+import { AnimatedBackground } from "./AnimatedBackground";
 import { ContactList } from "./ContactsList";
 import { LinkList } from "./LinkList";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="title">
-        Вітаю! <br />
-        Мене звати Анастасія!
-      </h1>
-      <h2 className="subtitle">
-        В минулому я хореограф та тренер зі стретчингу, а зараз - світчер
-        fullstack developer
-      </h2>
-      <div className="sections">
-        <section className="section">
-          <h3 className="subsubtitle">Далі посилання:</h3>
-          <p className="comment">* всі елементи клікабельні</p>
-          <LinkList />
-        </section>
+    <>
+      <AnimatedBackground />
+      <div className="container content">
+        <h1 className="title">
+          Вітаю! <br />
+          Мене звати Анастасія!
+        </h1>
+        <h2 className="subtitle">
+          В минулому я хореограф та тренер зі стретчингу, а зараз - світчер
+          fullstack developer
+        </h2>
+        <div className="sections">
+          <section className="section">
+            <h3 className="subsubtitle">Далі посилання:</h3>
+            <p className="comment">* всі елементи клікабельні</p>
+            <LinkList />
+          </section>
 
-        <section>
-          <h3 className="subsubtitle">
-            Звʼязатися зі мною можна будь-яким зручним для Вас способом:
-          </h3>
-          <p className="comment">* також всі елементи є клікабельними</p>
-          <ContactList />
-        </section>
+          <section>
+            <h3 className="subsubtitle">
+              Звʼязатися зі мною можна будь-яким зручним для Вас способом:
+            </h3>
+            <p className="comment">* також всі елементи є клікабельними</p>
+            <ContactList />
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
