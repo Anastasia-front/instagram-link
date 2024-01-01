63f6873e314f718f63c5cbe3b66023360d920d80
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-01-01 19:43:17 +0200

    fix : correct parallax effect

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -1,32 +1,60 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 export function AnimatedBackground() {
-  const mountains = useRef<HTMLDivElement | null>(null);
+  const [isTop, setIsTop] = useState(true);
 
-  useGSAP(
-    () => {
-      gsap.utils.toArray<HTMLDivElement>(".parallax").forEach((layer) => {
-        const depth = layer.dataset.depth;
-        const movement = -(layer.offsetHeight * parseFloat(depth || "1"));
-        gsap.to(layer, { y: movement, ease: "none" });
-      });
-    },
-    { dependencies: [mountains], scope: mountains, revertOnUpdate: true }
-  );
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.create({
+      trigger: "#bg-parallax",
+      start: "top top",
+      end: "bottom bottom",
+    });
+    gsap.to(".parallax", {
+      scrollTrigger: {
+        scrub: true,
+      },
+      y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+      ease: "none",
+    });
+
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      setIsTop(scrollTop === 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="mountains" ref={mountains}>
-      <div className="layer-bg layer parallax" data-depth="1"></div>
-      <div className="layer-1 layer parallax" data-depth="1"></div>
-      <div className="layer-2 layer parallax" data-depth="1"></div>
-      <div className="layer-3 layer parallax" data-depth="1"></div>
-      <div className="layer-overlay layer parallax" data-depth="1"></div>
-      <div className="layer-4 layer parallax" data-depth="1"></div>
-      <div className="layer-5 layer parallax" data-depth="1"></div>
-      <div className="layer-6 layer parallax" data-depth="1"></div>
+    <div id="bg-parallax">
+      <div className="mountains">
+        {isTop && <p>scroll down</p>}
+        <div className="layer-1 layer parallax" data-speed="1"></div>
+        <div className="layer-2 layer parallax" data-speed="1"></div>
+        <div className="layer-3 layer parallax" data-speed="0.3"></div>
+        <div className="layer-overlay layer parallax" data-speed="0.5"></div>
+        <div className="layer-4 layer parallax" data-speed="1"></div>
+        <div className="layer-5 layer parallax" data-speed="1"></div>
+        <div className="layer-6 layer parallax" data-speed="1"></div>
+      </div>
+      <div className="clouds">
+        <div className="layer-1 layer parallax" data-speed="0.6"></div>
+        <div className="layer-2 layer parallax" data-speed="0.8"></div>
+        <div className="layer-3 layer parallax" data-speed="1"></div>
+        <div className="layer-4 layer parallax" data-speed="0.7"></div>
+        <div className="layer-5 layer parallax" data-speed="1"></div>
+        <div className="layer-6 layer parallax" data-speed="0.9"></div>
+        <div className="layer-7 layer parallax" data-speed="1"></div>
+      </div>
     </div>
   );
 }
